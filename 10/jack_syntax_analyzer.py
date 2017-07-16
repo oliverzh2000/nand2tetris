1a34befa25bc8db37336c64b9c0642012e483046
Oliver Zhang <oliverzh2000@gmail.com>
2017-07-16 12:01:55 -0400

    Add XML writing to Jack Tokenizer

--- 10/jack_syntax_analyzer.py
+++ 10/jack_syntax_analyzer.py
@@ -1,48 +1,44 @@
 # Jack Syntax Analyzer. July 10. Oliver Zhang.
 
 import re
+import xml.etree.ElementTree as ET
 from collections import namedtuple
 
+a = "hello"
 # Define Lexical elements.
-KEYWORDS = ("class", "constructor", "function", "method", "field", "static", "var", "int", "char", "boolean", "void", "true", "false", "null", "this", "let", "do", "if", "else", "while", "return")
-SYMBOLS = "{}()[].,;+-*/&|<>=~"
-
-REGEX_MULTILINE_COMMENT = r"/\*.*\*/"   # Match '/* comment until closing * /'.
-REGEX_INLINE_COMMENT = r"//.*$"         # Match '// comment until end of line'.
-
-in_filename = "copy/ArrayTest/Main.jack"
-in_text = open(in_filename).read()
-
-# Remove comments and strip whitespace.
-in_text = re.sub(pattern=REGEX_MULTILINE_COMMENT, repl="", string=in_text, flags=re.DOTALL)
-in_text = re.sub(pattern=REGEX_INLINE_COMMENT, repl="", string=in_text, flags=re.MULTILINE)
-in_text = " ".join([line.strip() for line in in_text.splitlines() if line not in (" ", "")])
-
-print(in_text)
-print()
-
-# Tokenize.
+LEXICAL_TYPES = ("keyword", "symbol", "int_const", "str_const", "identifier")
+LEXICAL_ELEMENTS_REGEX = "{keyword}|{symbol}|{int_const}|{str_const}|{identifier}".format(
+    keyword="(class|constructor|function|method|field|static|var|int|char|boolean|void|true|false|null|this|let|do|if|else|while|return)",
+    symbol="([{}()[\].,;+\-*/&|<>=~])",
+    int_const="(\d+)",
+    str_const='"([^\n]*?)"',
+    identifier="([A-Za-z_]\w*)"
+)
 Token = namedtuple("Token", ['type', 'value'])
-tokens = []
 
-# Tokenize string constants first.
-for str_partition in re.split(pattern='(".*")', string=in_text):
-    if str_partition[0] == str_partition[-1] == '"':
-        tokens.append(Token(type="str_const", value=str_partition.strip('"')))
-    else:
-        # Tokenize symbols.
-        for symbol_partition in re.split(
-                pattern="([{}\s])".format(re.escape(SYMBOLS)), string=str_partition):
-            if symbol_partition in (" ", ""):
-                continue
-            if symbol_partition in KEYWORDS:
-                tokens.append(Token(type="keyword", value=symbol_partition))
-            elif symbol_partition in SYMBOLS:
-                tokens.append(Token(type="symbol", value=symbol_partition))
-            elif symbol_partition.isnumeric():
-                tokens.append(Token(type="int_const", value=symbol_partition))
-            elif symbol_partition.replace("_", "").isalnum():
-                tokens.append(Token(type="id", value=symbol_partition))
 
-for token in tokens:
-    print(token)
+def tokenize(in_filename, output_xml=True):
+    """Generate tokens for the input file based on the lexical elements in the Jack language."""
+
+    tokens = []
+
+    in_text = open(in_filename).read()
+    # Remove '/* comment until closing * /' and '// comment until end of line'.
+    in_text_no_comments = re.sub(pattern=r"/\*.*?\*/|//[^\n]*\n", repl="", string=in_text, flags=re.DOTALL)
+
+    for match_group in re.findall(LEXICAL_ELEMENTS_REGEX, in_text_no_comments):
+        for index, match in enumerate(match_group):
+            if match != "":
+                tokens.append(Token(type=LEXICAL_TYPES[index], value=match))
+
+    if output_xml:
+        root = ET.Element('tokens')
+        tree = ET.ElementTree(root)
+        for token in tokens:
+            token_element = ET.SubElement(root, token.type)
+            token_element.text = token.value
+        tree.write(in_filename.rstrip(".jack") + "T.xml")
+    return tokens
+
+
+print(tokenize("ArrayTest/Main.jack"))
